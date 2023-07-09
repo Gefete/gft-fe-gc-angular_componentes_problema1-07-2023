23e875dff6669cba14b9a00156756f9a56d12ef6
Gerard Fernandez Tejada <84667512+Gefete@users.noreply.github.com>
2023-07-09 17:18:21 +0200

    funcionalmente completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gft-fe-gc-angular_componentes_problema1-07-2023';
+  nombre = '';
+  cif = '';
+  direccion = '';
+  grupo = '';
+  clientes:any = [];
+
+
+  addCliente(){
+    this.clientes.push({
+      'nombre' : this.nombre,
+      'cif' : this.cif,
+      'direccion' : this.direccion,
+      'grupo' : this.grupo
+    });
+  }
 }
---
+++ src/app/clientes/clientes.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.css']
+})
+export class ClientesComponent {
+  
+  @Input() data:any;
+
+  
+}
